79f844d8091e8482b4249554190634ca899a3da5
jiapei.xie <2170928274@qq.com>
2020-12-21 09:41:26 +0800

    Demo

---
+++ ImagePicker/ImagePicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImagePicker
+//
+//  Created by 谢佳培 on 2020/12/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ImagePicker/ImagePicker/AppDelegate.m
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.m
+//  ImagePicker
+//
+//  Created by 谢佳培 on 2020/12/21.
+//
+
+#import "AppDelegate.h"
+#import "UIImagePickerControllerDemo.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    UIImagePickerControllerDemo *rootVC = [[UIImagePickerControllerDemo alloc] init];
+    UINavigationController *mainNC = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = mainNC;
+    [self.window makeKeyAndVisible];
+    
+    return YES;
+}
+
+@end
